9207e7f09bf23a9f4bd62717c11f3f5033cc8041
35HaRRy <hayrihabip@hotmail.com>
2015-12-02 01:37:35 +0200

    - servisdeki hatalar giderildi

--- ExchangeNotifier/service.py
+++ ExchangeNotifier/service.py
@@ -4,19 +4,27 @@ import simplejson as json
 
 from datetime import datetime as dt
 
+webConfig = json.loads(open("C:\\Users\\hayri\\Desktop\\IOT\\ExchangeNotifier\\ExchangeNotifier\\webConfig.json").read())
+
+currencyQueryStartHour = webConfig["CurrencyQueryStartHour"].split(":")
+currencyQueryFinishHour = webConfig["CurrencyQueryFinishHour"].split(":")
+
+for i in range(0, 1):
+    currencyQueryStartHour[i] = (int)(currencyQueryStartHour[i])
+    currencyQueryFinishHour[i] = (int)(currencyQueryFinishHour[i])
+
 while True:
-    webConfig = json.loads(open("C:\\Users\\hayri\\Desktop\\IOT\\ExchangeNotifier\\ExchangeNotifier\\webConfig.json").read())
+    now = dt.now()
 
     r = requests.get(webConfig["CurrencyQueryUrl"])
     print(r.text)
 
-    now = dt.now()
-    currencyQueryFinishHour = webConfig["CurrencyQueryFinishHour"].split(":")
-    currencyQueryStartHour = webConfig["CurrencyQueryStartHour"].split(":")
-
     sleepTime = (int)(webConfig["CurrencyQueryPeriod"])
-    if now.hour == (int)(currencyQueryFinishHour[0]) and (int)(currencyQueryFinishHour[0]) - 1 <= now <= (int)(currencyQueryFinishHour[0]) + 1:
-        sleepTime = (24 + (int)(currencyQueryStartHour[0]) - now.hour) * 60 + (int)(currencyQueryStartHour[1]) - now.minute
+    if now.hour == currencyQueryFinishHour[0] and currencyQueryFinishHour[1] - 1 <= now.minute <= currencyQueryFinishHour[1] + 1:
+        sleepTime = (24 + currencyQueryStartHour[0] - now.hour) * 60 + currencyQueryStartHour[1] - now.minute
+
+        if now.isocalendar()[2] - 1 == 5: # cuma gunu ise pazartesi acilisa kadar uyu
+            sleepTime += 48
 
     print(sleepTime.__str__() + " sn. uyku modunda")
     time.sleep(sleepTime)
